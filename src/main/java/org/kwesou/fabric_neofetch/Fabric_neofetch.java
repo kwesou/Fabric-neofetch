a533a59291867710d079ff8917660a088b939951
kwesou <103958556+kwesou@users.noreply.github.com>
2025-04-07 17:19:31 +0200

    Added fastfetch support

--- src/main/java/org/kwesou/fabric_neofetch/Fabric_neofetch.java
+++ src/main/java/org/kwesou/fabric_neofetch/Fabric_neofetch.java
@@ -13,13 +13,31 @@ import java.io.InputStreamReader;
 import static net.minecraft.server.command.CommandManager.*;
 
 public class Fabric_neofetch implements ModInitializer {
-    StringBuilder neofetchOutput;
+    StringBuilder neofetchOutput = new StringBuilder();
+    Process neofetchProcess;
     public static final Logger LOGGER = LoggerFactory.getLogger("org.kwesou.fabric_neofetch");
 
     public String executeNeoFetchCommand() {
         try {
-            neofetchOutput = new StringBuilder();
-            Process neofetchProcess = new ProcessBuilder("neofetch", "--stdout", "--disable", "packages", "shell", "theme", "icons").start();
+            try {
+                neofetchProcess = new ProcessBuilder(
+                        "fastfetch",
+                        "--pipe",
+                        "--logo", "none",
+                        "--structure", "title:separator:os:kernel:uptime:terminal:cpu:gpu:memory"
+                ).start();
+            } catch (IOException e) {
+                try {
+                    neofetchProcess = new ProcessBuilder(
+                            "neofetch",
+                            "--stdout",
+                            "--disable", "packages", "shell", "theme", "icons"
+                    ).start();
+                } catch (IOException ex) {
+                    throw new RuntimeException(ex);
+                }
+            }
+
             BufferedReader reader = new BufferedReader(new InputStreamReader(neofetchProcess.getInputStream()));
             neofetchProcess.waitFor();
             String line;
@@ -30,7 +48,7 @@ public class Fabric_neofetch implements ModInitializer {
             neofetchOutput = new StringBuilder("neofetch failed");
             LOGGER.warn(exception.getMessage());
         }
-        return neofetchOutput.toString();
+        return String.valueOf(neofetchOutput);
     }
 
     @Override
@@ -38,6 +56,7 @@ public class Fabric_neofetch implements ModInitializer {
         CommandRegistrationCallback.EVENT.register((dispatcher, registryAccess, environment) -> dispatcher.register(literal("neofetch")
                 .executes(context -> {
                     context.getSource().sendFeedback(() -> Text.literal(executeNeoFetchCommand()), false);
+                    neofetchOutput.setLength(0);
                     return 1;
                 })));
     }
